8525535f23be8cc099f4931d9f9bf5de92f03f3e
TrollFacedGamer <bwang@brooklynsteamcenter.org>
2025-02-14 10:52:58 -0500

    DataBase Connection

--- main.py
+++ main.py
@@ -1,9 +1,112 @@
 from flask import Flask, render_template, request, redirect, flash, abort
-
+import pymysql
+from dynaconf import Dynaconf
+import flask_login
+import datetime
 
 app = Flask(__name__) 
 
+conf = Dynaconf(
+    settings_file = ["settings.toml"]
+)
 
+app.secret_key = conf.secret_key
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = ('/sign_in')
+
+def connect_db():
+    conn =pymysql.connect(
+        host="db.steamcenter.tech",
+        database="bwang_streamline_water",
+        user='bwang',
+        password= conf.password,
+        autocommit= True,
+        cursorclass= pymysql.cursors.DictCursor
+    )
+
+    return conn
+
+class User:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+
+    def __init__(self, id, username, email, first_name, last_name):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def get_id(self):
+        return str(self.id)
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Customer` WHERE `id` = {user_id}")
+
+    result = cursor.fetchone()
+    #if there is no vaule in the requested database vaule a None will be returned
+    cursor.close()
+    conn.close()
+
+    if result is not None:
+        return User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
+
+
+#coordinator connect two fuction
 @app.route("/")
 def index():
-    return render_template("home_page.html.jinja")
+    return render_template("home_page.html.jinja",)
+
+@app.route("/sign_in")
+def sign_in_page():
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.close()
+    conn.close()
+
+@app.route("/map")
+@flask_login.login_required
+def map_page():
+    conn = connect_db()
+    cursor = conn.cursor()
+    customer_id = flask_login.current_user.id
+
+    cursor.close()
+    conn.close()
+
+
+# @app.route("/cart")
+# @flask_login.login_required
+# def cart_page():
+#     conn = connect_db()
+#     cursor = conn.cursor()
+#     customer_id = flask_login.current_user.id
+    # cursor.execute(f"SELECT * FROM `Customer` WHERE `username` = '{username}' ;")
+    #     #there are '' around {username} because its a string
+
+    #     result = cursor.fetchone()
+#     cursor.close()
+#     conn.close()
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect('/')
